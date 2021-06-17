05d1c72b9c61c5078994b11d5e85bf182fdaa5e3
gmartinonQM <gmartinon@quantmetry.com>
2021-06-17 11:24:12 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 rm -f how_to_opensource/core.py
+rm -f how_to_opensource/_version.py
 rm -f mypy.ini
 rm -f how_to_opensource/tests/test_core.py
-rm -f .github/workflows/python-package-conda.yml
+rm -f .github/workflows/*
 rm -rf doc
 rm -f .readthedocs.yml
 rm -f setup.py
-rm -f how_to_opensource/_version.py
 rm -f .bumpversion.cfg
