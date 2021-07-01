1f8a5a78c166028160f3f2b64489284cba9d679a
gmartinonQM <gmartinon@quantmetry.com>
2021-07-01 21:38:28 +0200

    Bump version: 0.0.2 → 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="QM How to Opensource by GMA",
-    version="0.0.2",
+    version="1.0.0",
     author="Grégoire Martinon, Vianney Taquet, Damien Hervault",
     author_email="gmartignon@quantmetry.com",
     description="A Quantmetry tutorial on how to publish an opensource python package.",
