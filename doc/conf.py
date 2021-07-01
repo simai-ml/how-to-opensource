4330c0a6cb373794cf319e08143cee450680bb6e
gmartinonQM <gmartinon@quantmetry.com>
2021-07-01 21:41:55 +0200

    fix version

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Grégoire Martinon, Vianney Taquet, Damien Hervault'
 author = 'Grégoire Martinon, Vianney Taquet, Damien Hervault'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.0.0'
 
 
 # -- General configuration ---------------------------------------------------
