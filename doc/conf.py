00d8e010cdb06db06974471ffe0e0f0ff18e5e1b
gmartinonQM <gmartinon@quantmetry.com>
2021-07-01 21:23:50 +0200

    Bump version: 0.0.0 → 0.0.1

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Grégoire Martinon, Vianney Taquet, Damien Hervault'
 author = 'Grégoire Martinon, Vianney Taquet, Damien Hervault'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
